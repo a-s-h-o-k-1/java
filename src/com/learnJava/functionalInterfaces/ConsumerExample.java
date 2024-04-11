f0ea57e5db8205b831ac6f87145c0f31964de68c
Ashok <2076320@cognizant.com>
2024-04-11 14:53:46 +0530

    removed code duplication

--- src/com/learnJava/functionalInterfaces/ConsumerExample.java
+++ src/com/learnJava/functionalInterfaces/ConsumerExample.java
@@ -8,6 +8,13 @@ import java.util.List;
 import java.util.function.Consumer;
 
 public class ConsumerExample {
+    static Consumer<Student> c1 = (s) -> System.out.println(s.getName());
+    static Consumer<Student> c2 = (s) -> System.out.print(s.getName());
+    static Consumer<Student> c3 = (s) -> System.out.println(s.getActivities());
+    static Consumer<Student> c4 = (s) -> {
+        if (s.getGradeLevel() >= 3 && s.getGpa() >= 3.9)
+            System.out.println(s);
+    };
 
     public static void main(String[] args) {
         //real world example
@@ -19,29 +26,20 @@ public class ConsumerExample {
 
     public static void printStudents() {
         System.out.println("printStudents:");
-        Consumer<Student> c1 = (s) -> System.out.println(s.getName());
         List<Student> students = StudentDataBase.getAllStudents();
         students.forEach(c1);
     }
 
     public static void printStudentNamesAndActivities() {
         System.out.println("printStudentNamesAndActivities:");
-        Consumer<Student> c2 = (s) -> System.out.print(s.getName());
-        Consumer<Student> c3 = (s) -> System.out.println(s.getActivities());
         List<Student> students = StudentDataBase.getAllStudents();
         students.forEach(c2.andThen(c3));
     }
 
     public static void printStudentsWithSpecificGrade() {
         System.out.println("printStudentsWithSpecificGrade:");
-
-        Consumer<Student> c4 = (s) -> {
-            if (s.getGradeLevel() >= 3 && s.getGpa()>=3.9)
-                System.out.println(s);
-        };
         List<Student> students = StudentDataBase.getAllStudents();
         students.forEach(c4);
     }
-
 
 }
