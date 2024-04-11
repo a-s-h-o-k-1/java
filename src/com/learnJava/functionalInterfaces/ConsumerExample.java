c1bba9f74d324ee51a1170b0db036176ce185d15
Ashok <2076320@cognizant.com>
2024-04-11 14:39:52 +0530

    added Consumer fi for gettting studentdata

--- src/com/learnJava/functionalInterfaces/ConsumerExample.java
+++ src/com/learnJava/functionalInterfaces/ConsumerExample.java
@@ -1,3 +1,23 @@
 package com.learnJava.functionalInterfaces;
+
+import com.learnJava.data.Student;
+import com.learnJava.data.StudentDataBase;
+
+import javax.swing.*;
+import java.util.List;
+import java.util.function.Consumer;
+
 public class ConsumerExample {
+
+    public static void main(String[] args) {
+        //real world example
+        printStudents();
+
+    }
+
+    public static void printStudents() {
+        Consumer<Student> c1 = (s) -> System.out.println(s.getName());
+        List<Student> students = StudentDataBase.getAllStudents();
+        students.forEach(c1);
+    }
 }
