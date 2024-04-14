15c7ae974ae1912d0d980e724c39b862a4553992
Ashok <2076320@cognizant.com>
2024-04-14 18:55:22 +0530

    added bipredicate example

--- src/com/learnJava/functionalInterfaces/PredicateAndConsumerExample.java
+++ src/com/learnJava/functionalInterfaces/PredicateAndConsumerExample.java
@@ -5,6 +5,7 @@ import com.learnJava.data.StudentDataBase;
 
 import java.util.List;
 import java.util.function.BiConsumer;
+import java.util.function.BiPredicate;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
 
@@ -12,11 +13,17 @@ public class PredicateAndConsumerExample {
 
     Predicate<Student> p1 = stu -> stu.getGpa() >= 3.9;
     Predicate<Student> p2 = stu -> stu.getGradeLevel() >= 3;
+    BiPredicate<Integer,Double> bp = (gradeLevel, gpa) -> gradeLevel>=3 && gpa>=3.9;
     BiConsumer<String, List<String>> studentNameandActivities = (name, activities) -> {
         System.out.println(name + "   :   " + activities);
     };
     Consumer<Student> c1 = stu -> {
         if (p1.and(p2).test(stu)) {
+            studentNameandActivities.accept(stu.getName(), stu.getActivities());
+        }
+    };
+    Consumer<Student> c2 = stu -> {
+        if (bp.test(stu.getGradeLevel(),stu.getGpa())) {
             studentNameandActivities.accept(stu.getName(), stu.getActivities());
         }
     };
@@ -28,6 +35,7 @@ public class PredicateAndConsumerExample {
     }
 
     public  void printNamesAndActivities(List<Student> students) {
-        students.forEach(c1);
+      //  students.forEach(c1);
+        students.forEach(c2);
     }
 }
